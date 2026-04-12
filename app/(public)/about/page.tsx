715d2c8fe12a5967309a0026065eddbcae17d156
Bipin Mishra <bipinmm165@gmail.com>
2026-04-13 00:11:39 +0530

    Updated About Page

--- app/(public)/about/page.tsx
+++ app/(public)/about/page.tsx
@@ -1,3 +1,36 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Page",
+
+  description: "description of the about page",
+
+  keywords: [],
+
+  openGraph: {
+    title: "sharing title for about page",
+    description: "sharing description for about page",
+    url: "https://website.com/about",
+    siteName: "site name",
+    type: "website",
+    images: [
+      {
+        url: "/image.webp",
+        width: 1200,
+        height: 630,
+        alt: "alt text for image",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "sharing title for about page",
+    description: "sharing description for about page",
+    images: ["/image.webp"],
+  },
+};
+
 const page = () => {
   return <div>page</div>;
 };
