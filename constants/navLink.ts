5f1d84758720cd8e075bcbf264934b30fc1e98a1
Bipin Mishra <bipinmm165@gmail.com>
2026-04-12 23:59:23 +0530

    Added navLinks file

---
+++ constants/navLink.ts
@@ -0,0 +1,15 @@
+export interface NavLink {
+  name: string;
+  href: string;
+}
+
+export const navLinks: NavLink[] = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "About",
+    href: "/about",
+  },
+];
